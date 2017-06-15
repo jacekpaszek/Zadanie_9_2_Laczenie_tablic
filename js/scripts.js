b38b6b074a8298a708d4681335ed50732afd4130
Jacek <jacek.paszek@onet.pl>
2017-06-15 20:11:28 +0200

    layout modification

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
 var newName = 'Marian';
+
 if (allNames.indexOf(newName) === -1) {
   allNames.push(newName);
   console.log(allNames);
